fc720acb86ceb9a10517eb5a8a49c8ae727087c9
MrHidalgo <Luckyhidalgo@gmail.com>
2024-10-27 00:45:56 +0300

    Fixed build files.

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,23 @@ import legacy from '@vitejs/plugin-legacy';
 import { sync } from 'glob';
 
 export default {
+  base: './',
   root: './src',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
     rollupOptions: {
-      input: sync('./src/**/*.html'.replaceAll('\\', '/'))
+      input: sync('./src/**/*.html'.replaceAll('\\', '/')),
+      output: {
+        entryFileNames: 'js/[name]-[hash].js',
+        chunkFileNames: 'js/[name]-[hash].js',
+        assetFileNames: (assetInfo) => {
+          if (/\.css$/.test(assetInfo.name)) {
+            return 'css/[name]-[hash][extname]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        }
+      }
     }
   },
   plugins: [legacy()]
