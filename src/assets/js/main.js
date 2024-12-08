5b8d7aadf096e6758f8010aa06810870ac2f6c06
MrHidalgo <Luckyhidalgo@gmail.com>
2024-12-08 08:52:22 +0200

    Fixed .prettierrc & .eslintrc files.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,10 +1,1 @@
-import { loadSection } from './utilities/loadComponent.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  loadSection('#header', './template/components/header.html', undefined, () => {
-    console.log('Header loaded callback');
-  });
-  loadSection('#footer', './template/components/footer.html', undefined, () => {
-    console.log('Footer loaded callback');
-  });
-});
+document.addEventListener('DOMContentLoaded', () => {});
